b405c04a91d9e0b21bd4a117d49cb14dd0fc87ef
Yogie Putra <yogie.putra8@gmail.com>
2017-10-07 21:52:03 +0700

    add delete method

--- app.js
+++ app.js
@@ -17,11 +17,25 @@ app.post('/register', function (req, res) {
 	const usr = req.body.username
 	const pwd = req.body.password
 	console.log('username: ' + usr + ' and password: ' + pwd)
-	res.send('username: ' + usr + ' and password: ' + pwd)
+	res.status(200).json({ 
+		username: usr,
+		password: pwd,
+		message: 'success' 
+	})
 })
 
+app.put('/reset_password', function (req, res) {
+	const usr = req.body.username
+	const pwd = req.body.password
+	console.log('Success Reset password: username: ' + usr + ' and password: ' + pwd)
+	res.send('Success Reset password: username: ' + usr + ' and password: ' + pwd)
+})
 
-
+app.delete('/remove_id/:id', function (req, res) {
+	const id = req.params.id
+	console.log('Success remove id: ' + id)
+	res.send('Success remove id: ' + id)
+})
 
 
 
